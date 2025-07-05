e7d7206533ad5b7bf93bdc1cf523510263c01931
Abdul-SS <abdul.a@softsuave.com>
2025-07-05 19:36:00 +0530

    --> Tick icon

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,10 +2,12 @@ import EyeClosed from './EyeClosed.svg';
 import EyeOpen from './EyeOpen.svg';
 import GoogleIcon from './GoogleIcon.svg'
 import ArrowIcon from './ArrowIcon.svg';
+import TickIcon  from './TickIcon.svg';
 
 export const SVG = {
 EyeClosed,
 EyeOpen,
 GoogleIcon,
 ArrowIcon,
+TickIcon,
 }
